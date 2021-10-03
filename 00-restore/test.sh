6a157189088970bbeb15a13a50d3a1a758be8bfd
mastercoms <mastercoms@tuta.io>
2021-10-03 09:59:01 -0400

    restore script

---
+++ 00-restore/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "${BINDIR}"
+
+echo "Setting checkout.workers=0"
+git config checkout.workers 0
+echo "Restoring to old commit"
+git restore -s 5a58c6a7d88cf18242220d07ce1f5fc506cd8b66 -- test.txt
+git status
+echo "Restoring to HEAD"
+git restore -- test.txt
+git status
+echo "Updating index"
+git update-index test.txt
+git status
